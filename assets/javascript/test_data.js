2b86725bb3e08bbcb2447b2f7be91e7ce4dde120
Wendy Baum <wbaum52@gmail.com>
2020-02-02 21:46:40 -0500

    initial js, add test data them, add button to html for testing

---
+++ assets/javascript/test_data.js
@@ -0,0 +1,28 @@
+let reviews = {
+
+    review : [
+         {
+            id: "3169854723",
+            book: {
+            id: 58156,
+            isbn: "0782127002",
+            isbn13: "9780782127003",
+            text_reviews_count: 0,
+            uri: "kca://book/amzn1.gr.book.v1.OM5d9zwk6lOFDQpZafmuqA",
+            title: "Complete Java 2 Certification Study Guide",
+            title_without_series: "Complete Java 2 Certification Study Guide",
+            image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+            small_image_url: "https://s.gr-assets.com/assets/nophoto/book/50x75-a91bf249278a81aabab721ef782c4a74.png",
+            large_image_url: [],
+            link: "https://www.goodreads.com/book/show/58156.Complete_Java_2_Certification_Study_Guide",               
+            date_added: "Fri Jan 31 14:40:54 -0800 2020",            
+            url: "https://www.goodreads.com/review/show/3169854723",           
+            owned: "0"
+            }
+        }
+      ]
+ };
+
+
+
+     
