a14c83bf873cfd36437c8b197f84aef14bd615d0
Wendy Baum <wbaum52@gmail.com>
2020-02-03 21:53:13 -0500

    switch to google books api

--- assets/javascript/test_data.js
+++ assets/javascript/test_data.js
@@ -1,27 +1,193 @@
-let reviews = {
+let books =
 
-    review : [
-         {
-            id: "3169854723",
-            book: {
-            id: 58156,
-            isbn: "0782127002",
-            isbn13: "9780782127003",
-            text_reviews_count: 0,
-            uri: "kca://book/amzn1.gr.book.v1.OM5d9zwk6lOFDQpZafmuqA",
-            title: "Complete Java 2 Certification Study Guide",
-            title_without_series: "Complete Java 2 Certification Study Guide",
-            image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
-            small_image_url: "https://s.gr-assets.com/assets/nophoto/book/50x75-a91bf249278a81aabab721ef782c4a74.png",
-            large_image_url: [],
-            link: "https://www.goodreads.com/book/show/58156.Complete_Java_2_Certification_Study_Guide",               
-            date_added: "Fri Jan 31 14:40:54 -0800 2020",            
-            url: "https://www.goodreads.com/review/show/3169854723",           
-            owned: "0"
-            }
-        }
-      ]
- };
+{
+  "kind": "books#volumes",
+  "totalItems": 2,
+  "items": [
+   {
+    "kind": "books#volume",
+    "id": "9U5I_tskq9MC",
+    "etag": "DgM9ICGZ0Hk",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/9U5I_tskq9MC",
+    "volumeInfo": {
+     "title": "Eloquent JavaScript",
+     "subtitle": "A Modern Introduction to Programming",
+     "authors": [
+      "Marijn Haverbeke"
+     ],
+     "publisher": "No Starch Press",
+     "publishedDate": "2011",
+     "description": "Provides information and examples on writing JavaScript code, covering such topics as syntax, control, data, regular expressions, and scripting.",
+     "industryIdentifiers": [
+      {
+       "type": "ISBN_13",
+       "identifier": "9781593272821"
+      },
+      {
+       "type": "ISBN_10",
+       "identifier": "1593272820"
+      }
+     ],
+     "readingModes": {
+      "text": true,
+      "image": true
+     },
+     "pageCount": 224,
+     "printType": "BOOK",
+     "categories": [
+      "COMPUTERS"
+     ],
+     "averageRating": 4.0,
+     "ratingsCount": 6,
+     "maturityRating": "NOT_MATURE",
+     "allowAnonLogging": true,
+     "contentVersion": "0.1.1.0.preview.3",
+     "panelizationSummary": {
+      "containsEpubBubbles": false,
+      "containsImageBubbles": false
+     },
+     "imageLinks": {
+      "smallThumbnail": "http://books.google.com/books/content?id=9U5I_tskq9MC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+      "thumbnail": "http://books.google.com/books/content?id=9U5I_tskq9MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+     },
+     "previewLink": "http://books.google.com/books?id=9U5I_tskq9MC&printsec=frontcover&hl=&source=gbs_api",
+     "infoLink": "http://books.google.com/books?id=9U5I_tskq9MC&hl=&source=gbs_api",
+     "canonicalVolumeLink": "https://books.google.com/books/about/Eloquent_JavaScript.html?hl=&id=9U5I_tskq9MC"
+    },
+    "layerInfo": {
+     "layers": [
+      {
+       "layerId": "geo",
+       "volumeAnnotationsVersion": "4"
+      }
+     ]
+    },
+    "saleInfo": {
+     "country": "US",
+     "saleability": "NOT_FOR_SALE",
+     "isEbook": false
+    },
+    "accessInfo": {
+     "country": "US",
+     "viewability": "PARTIAL",
+     "embeddable": true,
+     "publicDomain": false,
+     "textToSpeechPermission": "ALLOWED",
+     "epub": {
+      "isAvailable": true
+     },
+     "pdf": {
+      "isAvailable": true
+     },
+     "webReaderLink": "http://play.google.com/books/reader?id=9U5I_tskq9MC&hl=&printsec=frontcover&source=gbs_api",
+     "accessViewStatus": "SAMPLE",
+     "quoteSharingAllowed": false
+    }
+   },
+   {
+    "kind": "books#volume",
+    "id": "yU8oDwAAQBAJ",
+    "etag": "VBeWCSlyxcY",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/yU8oDwAAQBAJ",
+    "volumeInfo": {
+     "title": "Implementing Oracle Integration Cloud Service",
+     "authors": [
+      "Robert van Molken",
+      "Phil Wilkins"
+     ],
+     "publisher": "Packt Publishing Ltd",
+     "publishedDate": "2017-01-20",
+     "description": "Understand everything you need to know about Oracle's Integration Cloud Service and how to utilize it optimally for your business About This Book The only guide to Integration Cloud Service in the market Focused on practical action to deliver ...",
+     "industryIdentifiers": [
+      {
+       "type": "ISBN_13",
+       "identifier": "9781786469564"
+      },
+      {
+       "type": "ISBN_10",
+       "identifier": "1786469561"
+      }
+     ],
+     "readingModes": {
+      "text": true,
+      "image": true
+     },
+     "pageCount": 506,
+     "printType": "BOOK",
+     "categories": [
+      "Computers"
+     ],
+     "maturityRating": "NOT_MATURE",
+     "allowAnonLogging": true,
+     "contentVersion": "0.1.1.0.preview.3",
+     "panelizationSummary": {
+      "containsEpubBubbles": false,
+      "containsImageBubbles": false
+     },
+     "imageLinks": {
+      "smallThumbnail": "http://books.google.com/books/content?id=yU8oDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+      "thumbnail": "http://books.google.com/books/content?id=yU8oDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+     },
+     "previewLink": "http://books.google.com/books?id=yU8oDwAAQBAJ&printsec=frontcover&hl=&source=gbs_api",
+     "infoLink": "https://play.google.com/store/books/details?id=yU8oDwAAQBAJ&source=gbs_api",
+     "canonicalVolumeLink": "https://play.google.com/store/books/details?id=yU8oDwAAQBAJ"
+    },
+    "layerInfo": {
+     "layers": [
+      {
+       "layerId": "geo",
+       "volumeAnnotationsVersion": "2"
+      }
+     ]
+    },
+    "saleInfo": {
+     "country": "US",
+     "saleability": "FOR_SALE",
+     "isEbook": true,
+     "listPrice": {
+      "amount": 43.99,
+      "currencyCode": "USD"
+     },
+     "retailPrice": {
+      "amount": 31.72,
+      "currencyCode": "USD"
+     },
+     "buyLink": "https://play.google.com/store/books/details?id=yU8oDwAAQBAJ&rdid=book-yU8oDwAAQBAJ&rdot=1&source=gbs_api",
+     "offers": [
+      {
+       "finskyOfferType": 1,
+       "listPrice": {
+        "amountInMicros": 4.399E7,
+        "currencyCode": "USD"
+       },
+       "retailPrice": {
+        "amountInMicros": 3.172E7,
+        "currencyCode": "USD"
+       },
+       "giftable": true
+      }
+     ]
+    },
+    "accessInfo": {
+     "country": "US",
+     "viewability": "PARTIAL",
+     "embeddable": true,
+     "publicDomain": false,
+     "textToSpeechPermission": "ALLOWED",
+     "epub": {
+      "isAvailable": true
+     },
+     "pdf": {
+      "isAvailable": true
+     },
+     "webReaderLink": "http://play.google.com/books/reader?id=yU8oDwAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+     "accessViewStatus": "SAMPLE",
+     "quoteSharingAllowed": false
+    }
+   }
+  ]
+ }
+ 
 
 
 
